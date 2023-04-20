0d67e656f9b3792557590704940b0ebad85d9008
Sah Menz <mensahemma670@gmail.com>
2023-04-20 22:58:57 +0100

    last setup

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Nav from './nav';
 
 function App() {
   return (
-    <div>
-      
-    </div>
-  )
+      <div>
+        <Nav />
+      </div>
+  );
 }
 
 export default App
---
+++ src/component/nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './nav.module.css'
+
+function Nav() {
+  return (
+    <div className={styles.header}>
+        
+    </div>
+  )
+}
+
+export default Nav
